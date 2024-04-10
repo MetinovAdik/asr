2a25d3c71fb259e5990747c999ec907a79af7a24
MetinovAdik <metinovadilet@gmail.com>
2024-04-11 04:38:55 +0600

    +response trim

--- src/main/java/com/metinov/asr/ASRservice.java
+++ src/main/java/com/metinov/asr/ASRservice.java
@@ -17,12 +17,20 @@ public class ASRservice {
         processBuilder.redirectErrorStream(true);
 
         Process process = processBuilder.start();
+        StringBuilder transcription = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream(), StandardCharsets.UTF_8))) {
             String line;
+            boolean isResultStart = false; // Флаг для отслеживания начала результата транскрипции
             while ((line = reader.readLine()) != null) {
-                System.out.println(line);
+                if (line.contains("—")) { // Предполагаем, что результат начинается после строки с "—"
+                    isResultStart = true;
+                }
+                if (isResultStart) {
+                    transcription.append(line).append("\n"); // Добавляем строку к результату
+                }
             }
         }
+        System.out.println(transcription.toString().trim());
     }
 
 }
